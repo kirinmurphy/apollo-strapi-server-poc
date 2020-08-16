c7e9081e2b3e12ba16a027b1ba0660e2118cfac4
Kirin <kirinmurphy@gmail.com>
2020-08-16 13:34:46 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9890fd57bf2538a1d42e73785c990b44'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8011adf8-ed97-40e1-b8df-f1e74e50f551'
+};
