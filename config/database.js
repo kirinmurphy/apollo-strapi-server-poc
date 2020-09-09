f4de422d90cf122deeae1371591de7eeecd9f961
Kirin <kirinmurphy@gmail.com>
2020-09-09 18:37:45 -0500

    and more db tweaks

--- config/database.js
+++ config/database.js
@@ -4,12 +4,8 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: "postgres",
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        database: process.env.DATABASE_NAME,
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
       options: {
         useNullAsDefault: true,
