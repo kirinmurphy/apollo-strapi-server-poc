a379758f688ccb445b1c1e52d87b7be3dc2f263e
Kirin <kirinmurphy@gmail.com>
2020-09-09 18:37:45 -0500

    and more db tweaks

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '9890fd57bf2538a1d42e73785c990b44'),
+      secret: env('ADMIN_JWT_SECRET')
     },
   },
 });
