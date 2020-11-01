cb83c2637fb4095ac3d286282e298f9568705a5e
Kirin <kirinmurphy@gmail.com>
2020-11-01 13:14:21 -0600

    added custom controller for impressions created by user

--- api/impression/controllers/impression.js
+++ api/impression/controllers/impression.js
@@ -5,4 +5,25 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async me(ctx) {
+    const user = ctx.state.user;    
+    // console.log('ctx user', user);
+    console.log('ctx state', ctx.state);
+    // console.log('ctx q', ctx.query);
+
+    if (!user) {
+      return ctx.badRequest(null, [
+        { messages: [{ id: 'No authorization header was found' }] }
+      ]);
+    }
+
+    const data = await strapi.services.impression.find({user:user.id});  
+
+    if(!data){
+      return ctx.notFound();
+    }
+
+    ctx.send(data);
+  },
+};
